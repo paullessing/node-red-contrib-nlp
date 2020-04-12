9067b6ba09cac502d75f2175219309f1f8b16bac
paullessing <paul@paullessing.com>
2020-04-12 18:57:05 +0100

    Update UI to have one input field per intent ID

--- src/intent.ts
+++ src/intent.ts
@@ -1,38 +1,15 @@
 import { Red, Node, NodeProperties } from './types/node-red';
 import { NlpManager } from 'node-nlp';
 
-interface UtteranceConfig {
-  utterance: string;
+interface IntentConfig {
+  utterances: string[];
   intent: string;
 }
 
+// TODO add a config setting for "emit only on match"
+
 export = function register(RED: Red) {
   RED.nodes.registerType('intent', IntentNode);
-
-  // // Adds the utterances and intents for the NLP
-  // manager.addDocument('en', 'turn the heating to %temperature%', 'heating_on');
-  // manager.addDocument('en', 'turn on the heating', 'heating_on');
-  // manager.addDocument('en', 'turn the heating on', 'heating_on');
-  // manager.addDocument('en', 'switch on the heating', 'heating_on');
-  // manager.addDocument('en', 'heating on', 'heating_on');
-  // manager.addDocument('en', 'warm up', 'heating_on');
-  // manager.addDocument('en', 'set heating to %temperature%', 'heating_on');
-  // manager.addDocument('en', 'heat to %temperature%', 'heating_on');
-  // manager.addDocument('en', 'start heating', 'heating_on');
-  // manager.addDocument('en', 'turn off the heating', 'heating_off');
-  // manager.addDocument('en', 'switch off the heating', 'heating_off');
-  // manager.addDocument('en', 'switch heating off', 'heating_off');
-  // manager.addDocument('en', 'heating off', 'heating_off');
-  // manager.addDocument('en', 'stop heating', 'heating_off');
-  // manager.addDocument('en', 'boost heating', 'heating_boost');
-  // manager.addDocument('en', 'boost heating for %duration%', 'heating_boost');
-  // manager.addDocument('en', 'boost heating to %temperature% for %duration%', 'heating_boost');
-  // manager.addDocument('en', 'heating boost 90m', 'heating_boost');
-  // manager.addDocument('en', 'boost the heating', 'heating_boost');
-  //
-  // // Train also the NLG
-  // manager.addAnswer('en', 'heating_on', 'Heating set to %temperature%');
-  // manager.addAnswer('en', 'heating_on', 'Heating turned on');
 
   // Train and save the model.
   function IntentNode(this: Node, config: NodeProperties) {
@@ -41,7 +18,9 @@ export = function register(RED: Red) {
 
     const manager = new NlpManager({ languages: ['en'], autoSave: false, autoLoad: false });
 
-    const utterances = parseUtterances(config.utterances);
+    console.log(config);
+
+    const utterances = parseUtterances(config.intents);
     utterances.forEach(({ utterance, intent }) => {
       manager.addDocument('en', utterance, intent);
     });
@@ -53,7 +32,6 @@ export = function register(RED: Red) {
         text: `${utterances.length} utterances`
       });
     });
-
 
     node.on('input', async (msg: any, send, done) => {
       // console.log('INPUT EVENT', msg, config);
@@ -68,21 +46,32 @@ export = function register(RED: Red) {
 
       send({
         ...msg,
-        payload: result.intent,
+        payload: {
+          intent: result.intent,
+          answer: result.answer,
+        },
         parseResult: result
       });
       done();
     });
 
-    function parseUtterances(configUtterances: string | UtteranceConfig[]): UtteranceConfig[] {
+    function parseUtterances(configUtterances: string | IntentConfig[]): { utterance: string, intent: string }[] {
       try {
-        const parsed: UtteranceConfig[] = typeof configUtterances === 'string' ?
+        const parsed: IntentConfig[] = typeof configUtterances === 'string' ?
           JSON.parse(configUtterances) :
           configUtterances;
-        return parsed.filter(({ utterance, intent }) => utterance && intent);
+        const utterances: { utterance: string, intent: string }[] = [];
+        parsed.forEach(({ utterances: _utterances, intent }) => {
+          if (_utterances && _utterances.length && intent) {
+            _utterances.forEach((utterance) => {
+              utterances.push({ intent, utterance });
+            })
+          }
+        });
+        return utterances;
       } catch (e) {
         return [];
       }
-    };
+    }
   }
 }
---
+++ test/test.js
@@ -0,0 +1,14 @@
+const { NlpManager } = require('node-nlp');
+
+const manager = new NlpManager({ languages: ['en'], autoSave: false, autoLoad: false });
+
+(async () => {
+  manager.addDocument('en', 'turn the heating to %number%', 'heating_on');
+  manager.addAnswer('en', 'heating_on', 'Turned to %number%');
+
+  await manager.train();
+
+  const result = await manager.process('turn the heating to 24');
+
+  console.log(JSON.stringify(result, 0, 2));
+})();
